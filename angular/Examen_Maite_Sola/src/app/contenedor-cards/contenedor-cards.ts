afb5b58f8ff5d29aa844ba521af2c15b30fecef9
Maite <maitesolam@gmail.com>
2025-11-13 12:31:27 +0100

    base

--- angular/Examen_Maite_Sola/src/app/contenedor-cards/contenedor-cards.ts
+++ angular/Examen_Maite_Sola/src/app/contenedor-cards/contenedor-cards.ts
@@ -13,30 +13,62 @@ export class ContenedorCards {
 
    // Array de pizzas como signal
   pizzas = signal<cardModel[]>([
-    {
-      nombre: 'Margarita',
-      precio: 8,
-      foto: '/img/margarita.jpg',
-      ingredientes: ['/img/tomate.jpg','/img/queso.jpg']
-    },
-    {
-      nombre: 'Pepperoni',
-      precio: 10,
-      foto: '/img/peperoni.jpg',
-      ingredientes: ['/img/tomate.jpg','/img/queso.jpg']
-    },
-    {
-      nombre: 'Barbacoa',
-      precio: 11,
-      foto: '/img/barbacoa.jpg',
-      ingredientes: ['/img/tomate.jpg','/img/queso.jpg']
-    },
-    {
-      nombre: 'Carbonara',
-      precio: 11,
-      foto: '/img/carbonara.jpg',
-      ingredientes: ['/img/tomate.jpg','/img/queso.jpg']
-    }
+   {
+    nombre: 'Margarita',
+    precio: 8.50,
+    // Usamos la versión .webp
+    foto: '/img/margarita.webp',
+    // Ingredientes inferidos
+    ingredientes: ['/img/tomate.png', '/img/queso.jpg'],
+  },
+  {
+    nombre: 'Pepperoni',
+    precio: 10.99,
+    // Usamos la versión .webp
+    foto: '/img/peperoni.webp',
+    // Ingredientes inferidos: Queso, pepperoni
+    ingredientes: ['/img/queso.jpg', '/img/peperoni.jpg'],
+  },
+  {
+    nombre: 'Barbacoa',
+    precio: 11.50,
+    // Usamos la versión .webp
+    foto: '/img/barbacoa.webp',
+    // Ingredientes inferidos: Queso, bacon, cebolla
+    ingredientes: ['/img/queso.jpg', '/img/bacon.jpg', '/img/cebolla.jpg'],
+  },
+  {
+    nombre: 'Carbonara',
+    precio: 11.90,
+    // Usamos la versión .jpg, ya que no hay .webp
+    foto: '/img/carbonara.jpg',
+    // Ingredientes inferidos: Queso, bacon, champis (champiñones)
+    ingredientes: ['/img/queso.jpg', '/img/bacon.jpg', '/img/champis.webp'],
+  },
+  {
+    nombre: 'Cuatro Quesos',
+    precio: 12.25,
+    // Usamos la versión .jpg, ya que no hay .webp
+    foto: '/img/4quesos.jpg',
+    // Ingredientes inferidos: Solo queso (por ser 4 quesos)
+    ingredientes: ['/img/queso.jpg'],
+  },
+  {
+    nombre: 'Vegetal',
+    precio: 9.75,
+    // Usamos la versión .jpg
+    foto: '/img/vegetal.jpg',
+    // Ingredientes inferidos: Tomate, cebolla, champiñones
+    ingredientes: ['/img/tomate.png', '/img/cebolla.jpg', '/img/champis.webp'],
+  },
+  {
+    nombre: 'Burrata',
+    precio: 13.00,
+    // Usamos la versión .webp
+    foto: '/img/burrata.webp',
+    // Ingredientes inferidos: Tomate, queso, burrata
+    ingredientes: ['/img/tomate.png', '/img/queso.jpg', '/img/burrata.jpg'],
+  }
   ]);
 
  // Maneja el evento de addToCart (ej. loguea, o usa un servicio de carrito)
